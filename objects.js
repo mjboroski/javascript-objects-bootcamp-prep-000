6535fe379ce3a8a2bf3aa57c5ad0388a84802e30
Michael Boroski <mjboroski@gmail.com>
2018-04-04 20:03:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {'Cake' : 'Sheep_Go_To_Heaven'};
 function updatePlaylist(playlist, artist, song){
     return Object.assign(playlist, playlist,{[artist]:song})
 }
+
+function removeFromPlaylist(playlist, artist){
+    delete playlist.artist;
+    return playlist;
+}
