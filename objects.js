59cd730ff21c71d01f19208af4958ffa312697c0
Michael Boroski <mjboroski@gmail.com>
2018-04-04 20:08:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ function updatePlaylist(playlist, artist, song){
 }
 
 function removeFromPlaylist(playlist, artist){
-    delete playlist.[artist];
+    holder=artist;
+    delete playlist.holder;
     return playlist;
 }
